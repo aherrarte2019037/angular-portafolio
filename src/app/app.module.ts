2e0ffc05e29eec620d3264c3b94f83f1b855903f
Angel Herrarte <angelherrarte3@gmail.com>
2024-05-19 14:26:38 -0600

    Create initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
